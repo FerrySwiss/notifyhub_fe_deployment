5f3a1ca9babd6e7b0c45e110f930b42fd3136271
arundaskr <arundas@ferryswiss.com>
2025-11-28 22:21:35 +0530

    feat: Update next.config.mjs rewrites for all REST and GraphQL endpoints

--- modernize-tailwind-nextjs-pro-main/packages/nextauth/next.config.mjs
+++ modernize-tailwind-nextjs-pro-main/packages/nextauth/next.config.mjs
@@ -9,6 +9,22 @@ const nextConfig = {
                 destination: 'http://localhost:8000/signup/:path*',
             },
             {
+                source: '/login/password/:path*',
+                destination: 'http://localhost:8000/login/password/:path*',
+            },
+            {
+                source: '/mfa/verify/:path*',
+                destination: 'http://localhost:8000/mfa/verify/:path*',
+            },
+            {
+                source: '/mfa/confirm/:path*',
+                destination: 'http://localhost:8000/mfa/confirm/:path*',
+            },
+            {
+                source: '/mfa/setup/:path*',
+                destination: 'http://localhost:8000/mfa/setup/:path*',
+            },
+            {
                 source: '/o/:path*',
                 destination: 'http://localhost:8000/o/:path*',
             },
