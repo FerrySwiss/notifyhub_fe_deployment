5bba2b6a33b2311fbde64b408996a8d146b24cf4
arundaskr <arundas@ferryswiss.com>
2025-11-28 21:39:22 +0530

    feat: Re-add proxy configuration to next.config.mjs for REST and GraphQL backend endpoints

--- modernize-tailwind-nextjs-pro-main/packages/nextauth/next.config.mjs
+++ modernize-tailwind-nextjs-pro-main/packages/nextauth/next.config.mjs
@@ -1,7 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
-    images: { unoptimized: true }
+    images: { unoptimized: true },
+    async rewrites() {
+        return [
+            {
+                source: '/signup/:path*',
+                destination: 'http://localhost:8000/signup/:path*',
+            },
+            {
+                source: '/o/:path*',
+                destination: 'http://localhost:8000/o/:path*',
+            },
+            {
+                source: '/graphql/:path*',
+                destination: 'http://localhost:8000/graphql/:path*',
+            },
+        ];
+    },
 };
 
 export default nextConfig;
